7ea53de615daa400578b501c53e2d01481125d74
josueSarabia <josuedavids@hotmail.com>
2022-08-30 14:42:27 -0500

    terraform RDS setup

---
+++ infrastructure/rds/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    profile = var.profile
+    region = "us-east-1"
+}
+
+resource "aws_db_instance" "default" {
+    allocate_storage = 20
+    storage_type = "gp2"
+    engine = "postgres"
+    instance_class = "db.t2.micro"
+    name = "contacts_db" // name of the db
+    username = "postgres"
+    password = var.password
+    identifier = var.id
+    // add security groups
+    // vpc_security_group_ids = [ "" ]
+    publicly_accessible = true
+}
---
+++ infrastructure/rds/variables.tf
@@ -0,0 +1,11 @@
+variable "profile" {
+    description = "profile to auth to Aws"
+}
+
+variable "password" {
+    description = "password for db"
+}
+
+variable "id" {
+    description = "friendly name of instance in aws console"
+}
